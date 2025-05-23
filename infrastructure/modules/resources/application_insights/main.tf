5564bec5558fb758dd4b4f01f4e127c224ba02b8
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-05-22 21:38:48 -0500

    Delete infrastructure/modules/resources/application_insights/main.tf

--- infrastructure/modules/resources/application_insights/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_application_insights" "ml" {
-  name                = "${var.prefix}-${var.env}-ml-ai"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  application_type    = "web"  # Required for ML workspace
-  retention_in_days   = 90     # Customize as needed
-
-  tags = var.tags
-}
