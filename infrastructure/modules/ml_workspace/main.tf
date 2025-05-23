ba0ad6dd392774fc3364a42eda3084e42ec4314f
DmaloneMN <87717617+DmaloneMN@users.noreply.github.com>
2025-05-22 21:27:26 -0500

    Update main.tf

--- infrastructure/modules/ml_workspace/main.tf
+++ infrastructure/modules/ml_workspace/main.tf
@@ -1,23 +1,42 @@
+resource "azurerm_application_insights" "ml_insights" {
+  name                = "${var.prefix}-${var.env}-ml-ai"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = "web"
+  retention_in_days   = var.app_insights_retention
+  tags                = var.tags
+}
+
+resource "azurerm_key_vault" "ml_kv" {
+  name                = "${replace(var.prefix, "-", "")}${var.env}mlkv" # Key Vault name rules
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+  purge_protection    = var.kv_purge_protection
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"
+    ]
+  }
+
+  tags = var.tags
+}
+
 resource "azurerm_machine_learning_workspace" "workspace" {
   name                    = "${var.prefix}-${var.env}-mlw"
   location                = var.location
   resource_group_name     = var.resource_group_name
-  application_insights_id = azurerm_application_insights.insights.id
-  key_vault_id            = azurerm_key_vault.kv.id
-  storage_account_id       = var.storage_account_id
+  application_insights_id = azurerm_application_insights.ml_insights.id
+  key_vault_id            = azurerm_key_vault.ml_kv.id
+  storage_account_id      = var.storage_account_id
 
   identity {
     type = "SystemAssigned"
   }
-}
 
-resource "azurerm_machine_learning_compute_cluster" "training" {
-  name                          = "cpu-cluster"
-  machine_learning_workspace_id = azurerm_machine_learning_workspace.workspace.id
-  vm_priority                   = "LowPriority"
-  vm_size                       = "Standard_DS3_v2"
-  scale_settings {
-    min_node_count = 0
-    max_node_count = 4
-  }
+  tags = var.tags
 }
